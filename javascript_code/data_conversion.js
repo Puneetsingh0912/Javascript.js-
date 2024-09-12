737d4f1f3de472c05afd1d6427cf70f6a1357c2e
Puneetsingh0912 <166594709+Puneetsingh0912@users.noreply.github.com>
2024-09-12 15:00:26 +0000

    data conversion

--- javascript_code/data_conversion.js
+++ javascript_code/data_conversion.js
@@ -8,7 +8,7 @@ console.log(typeof valueInNumber);
 // "33" => 33
 // "33abc" => NAN
 // true => 1; false => 0
-*/
+
 let isLoggedIn = ""
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
@@ -21,7 +21,53 @@ console.log(stringNumber1);
 console.log(stringNumber2);
 console.log(stringNumber1+stringNumber2);
 console.log(typeof stringNumber1);
-console.log(typeof stringNumber2);
+console.log(typeof stringNumber2);*/
+
+//****************************Operations**************************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+console.log(2+2);// add
+console.log(3-2);// subtract 
+console.log(3*3);// multiple
+console.log(4/2);// divison
+console.log(2**2);// power of 2 is 2
+console.log(3%2);// remdar
+
+let str1 = "hello"
+let str2 = "name"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter = 100
+++ gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
