b1ad96b1a607a828c1c9333162151fb18c4a3b84
Puneetsingh0912 <166594709+Puneetsingh0912@users.noreply.github.com>
2024-09-12 12:30:05 +0000

    data_types

--- .devcontainer/basics.js
+++ .devcontainer/basics.js
@@ -1,10 +1,10 @@
-const accountId = 144553
+const accountId = 144553 // not change data
 let accountEmail = "Puneetsingh0912@gmail.com"
 var accountPassword = "1234"
-accoountCity = "bharatpur"
-
-accountId = 2
+accoountCity = "bharatpur" 
  // account =2 // not allowed
+ /* prefer not use var because  of issu in block scope and function scope*/
+
  console.log(accountId);
  accountEmail='puneet0912@'
  accountPassword='123'
