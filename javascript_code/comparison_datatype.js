dc0d13f978ee9c5e832967787142a88ac1e21c55
Puneetsingh0912 <166594709+Puneetsingh0912@users.noreply.github.com>
2024-09-12 17:27:06 +0000

    comparison datatype

--- javascript_code/comparison_datatype.js
+++ javascript_code/comparison_datatype.js
@@ -1,10 +1,56 @@
-console.log(2>1);
+/*console.log(2>1);
 console.log(2>=1);
 console.log(2<1);
 console.log(2==1);
 console.log(2 !=1);
+*/
+
+//console.log("2">1);
+//console.log("02">1);
 
 
+/*console.log(null >0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);  
+
+// === cheak data type and magnitude
+console.log("2"===2);   */
+
+// # Primitive data type
+
+// 7 type : String, Number, Boolean, Null, Undefiend ,Symbol ,BigInt
 
 
+const scoreNumber = 100
+const scoreValue = 100.3
 
+const isLogged = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 345678912349348329293n
+
+// Reference {Non primitive}
+// Array, Objects, Function
+
+const heros = ["shaktiman", "naagraj", "doga"];
+ let myobj = {
+    name: "puneet",
+    age: 23,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+
+console.log( typeof  myFunction);
