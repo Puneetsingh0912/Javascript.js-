0f032c778a21ac30e90ac9a7f68d138ee20f7118
Puneetsingh0912 <166594709+Puneetsingh0912@users.noreply.github.com>
2024-09-12 11:00:28 +0000

    frist javascript program

---
+++ .devcontainer/basics.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "Puneetsingh0912@gmail.com"
+var accountPassword = "1234"
+accoountCity = "bharatpur"
+
+accountId = 2
+ // account =2 // not allowed
+ console.log(accountId);
+ accountEmail='puneet0912@'
+ accountPassword='123'
+ accoountCity='bharatpur'
+console.table([accountId,accountEmail,accountPassword,accoountCity]);
