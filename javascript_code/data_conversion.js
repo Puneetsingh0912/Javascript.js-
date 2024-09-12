33529634492f86613575bbbdb956d6db0a0d2585
Puneetsingh0912 <166594709+Puneetsingh0912@users.noreply.github.com>
2024-09-12 14:37:09 +0000

    data_conversion

--- javascript_code/data_conversion.js
+++ javascript_code/data_conversion.js
@@ -1,6 +1,27 @@
-let score = 33
+/*let score = 33
 
 console.log(typeof score);
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NAN
+// true => 1; false => 0
+*/
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1=> true; 0=> false; "" => false
+let someNumber1 = 33
+let someNumber2 = 34
+let stringNumber1 = String(someNumber1)
+let stringNumber2 = String(someNumber2)
+console.log(stringNumber1);
+console.log(stringNumber2);
+console.log(stringNumber1+stringNumber2);
+console.log(typeof stringNumber1);
+console.log(typeof stringNumber2);
+
+
+
