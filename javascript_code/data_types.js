18b81504462ac6f815775513b6ee4f921fa937e1
Puneetsingh0912 <166594709+Puneetsingh0912@users.noreply.github.com>
2024-09-12 17:46:16 +0000

    data_types

--- javascript_code/data_types.js
+++ javascript_code/data_types.js
@@ -1,3 +1,19 @@
+"use strict";// trea all js code as newer version
+// alert(3+3)// we are using nodejs,not brower
+console.log(3+3)
+// data type
+let name = "Puneet" // string
+let age = 18 // number
+let isLogged = false // boolean
+// bigint
+// null => stanadalone value
+ let state;// undefined
+ // symbol => unique
+
+ 
+// object
+
+console.log(typeof age);
 /*console.log(2>1);
 console.log(2>=1);
 console.log(2<1);
